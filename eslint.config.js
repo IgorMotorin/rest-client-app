fcf74426e325049806273da723651153d4fca820
jvallejoromero <jvallejoromero@csustan.edu>
2025-09-05 15:22:28 -0700

    chore: fix ESLint setup to ensure linting runs correctly

--- eslint.config.js
+++ eslint.config.js
@@ -1,46 +1,44 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import react from 'eslint-plugin-react';
-import tseslint from 'typescript-eslint';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 import reactCompiler from 'eslint-plugin-react-compiler';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
-export default tseslint.config(
-  { ignores: ['dist', '.next', 'next-env.d.ts'] },
+export default [
+  { ignores: ['dist', 'coverage', '.vercel', '.next', 'next-env.d.ts'] },
 
+  js.configs.recommended,
+  ...tseslint.configs.strict,
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.strict,
-      eslintPluginPrettier,
-    ],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tseslint.parser,
     },
     plugins: {
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       'react-compiler': reactCompiler,
+      prettier: prettierPlugin,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
       'react-compiler/react-compiler': 'error',
+      'prettier/prettier': 'error',
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  }
-);
+    settings: { react: { version: 'detect' } },
+  },
+  eslintConfigPrettier,
+];
