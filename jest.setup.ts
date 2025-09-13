9b1b745535f300c5e8f7686a725f6035a7f8c34d
jvallejoromero <jvallejoromero@csustan.edu>
2025-09-13 12:11:10 -0700

    chore(test): mock next-intl in jest setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom';
+
+jest.mock('next-intl', () => ({
+  __esModule: true,
+  useTranslations: () => {
+    return (nsOrKey: string) => {
+      return nsOrKey;
+    };
+  },
+}));
