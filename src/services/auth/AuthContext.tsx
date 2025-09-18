b2c373e1103baa6c33622ab597ba7edfa81b36d3
Igor <70837544+IgorMotorin@users.noreply.github.com>
2025-09-18 08:53:11 +0300

    Revert "Restful"

--- src/services/auth/AuthContext.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { createContext } from 'react';
-import { AuthContextValue } from '@/services/auth/auth.types';
-
-export const AuthContext = createContext<AuthContextValue | undefined>(
-  undefined
-);
